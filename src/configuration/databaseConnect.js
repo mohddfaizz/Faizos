632f2d9b9473342bfb6dfb81a10a7ccefc945edb
Mohd Faiz <43850905+mohddfaizz@users.noreply.github.com>
2024-11-17 21:31:11 +0530

    Update databaseConnect.js

--- src/configuration/databaseConnect.js
+++ src/configuration/databaseConnect.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
+const uri =  process.env.MONGODB_URI
+
+if (!uri) {
+  console.error('MongoDB URI is not defined');
+  process.exit(1); // Exit the process if Mongo URI is missing
+}
+
 const connectDatabase = async () => {
-    await mongoose.connect(
-        process.env.MONGODB_URI
-    );
+    await mongoose.connect(uri);
 };
 
 module.exports = connectDatabase;
